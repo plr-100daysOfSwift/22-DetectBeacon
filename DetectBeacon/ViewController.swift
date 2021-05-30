75d3d1fe040d02b5767ca1b38d38ec7c1dc051f3
Paul Richardson <developer@photographerafoot.com>
2021-05-31 09:29:36 +1000

    CHG: Set up interface
    A single label to display the distance reading

--- DetectBeacon/ViewController.swift
+++ DetectBeacon/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet var distanceReading: UILabel!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
